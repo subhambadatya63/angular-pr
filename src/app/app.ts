98079c474af0569dda5f48058d06ff983a2f8d53
Subhambadatya <subhambadatya1234@gmail.com>
2025-07-28 11:49:26 +0530

    no changes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,28 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Login } from './login/login';
+import { Signup } from './signup/signup';
+import { ProfileComponent } from './profile/profile.component';
+import { count } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('angular-pr');
+export class AppComponent {
+
+  count=0
+
+  handleIncrement() {
+    this.count=this.count+1
+  }
+  handleDecrement() {
+    this.count=this.count-1
+  }
+  handleReset() {
+    this.count=0
+  }
 }
+
---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'div',
+  template: '<h1>profile component</h1>',
+  //styles: 'h1{color:green}',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})  
+export class ProfileComponent {
+  // Component logic goes here
+}
